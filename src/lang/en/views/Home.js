44f18185baa95b53c47877307510b835ae685cb8
Kaplaugher <kaplaugher94@gmail.com>
2018-07-25 19:25:14 -0400

    slight update

--- src/lang/en/views/Home.js
+++ src/lang/en/views/Home.js
@@ -1,67 +1,77 @@
 export default {
-  jumbotronTitle: 'Build Better Software &mdash; Faster.',
-  jumbotronSubTitle: 'The complete suite of enterprise-level modern cloud software services',
-  callToActionBtn: 'Request a Quote',
-  platformHeader: 'The Enterprise-Scale Platform as a Service',
+  jumbotronTitle: "Welcome to Laurel Springs Animal Hospital",
+  jumbotronSubTitle: "We Love Them Like You Do.",
+  callToActionBtn: "Request a Quote",
+  platformHeader: "Why Choose Us?",
   cardFeatures: [
     {
-      title: 'Focus on Your Project',
-      text: 'With Alpha Software you don\'t need to worry about infrastructure, disaster recovery, or dev ops. Through the ASCLI tool everything from servers to orchestrated deployments is one click away.',
-      img: 'feature1'
+      title: "Superior Care",
+      text:
+        "At Laurel Springs Animal Hospital, we understand that your pet is a special part of your familly and we take the appropriate steps to...",
+      img: "feature1"
     },
     {
-      title: 'Support for Nine Languages',
-      text: 'Alpha Software products support 9 or more languages. Whether you are using Java, Node, C++, Swift, Go, or Ruby your applications are already compatible with Alpha Software\'s services.',
-      img: 'feature2'
+      title: "State of the art facilities",
+      text:
+        "lorem, lorem lorem lorem lorem lorem lorem lorem v lorem lorem lorem lorem lorem lorem",
+      img: "feature2"
     },
     {
-      title: 'Pricing from Startups to Enterprise',
-      text: 'At Alpha Software we know that startups and experimentations are a core component to the success and speed of innovation in the software industry. That\'s why we offer pricing from startups all the way to enterprise level businesses.',
-      img: 'feature3'
+      title: "Friendly and courteoous staff",
+      text:
+        "lorem, lorem lorem lorem lorem lorem lorem lorem v lorem lorem lorem lorem lorem lorem",
+      img: "feature3"
     }
   ],
   listFeatures: [
     {
-      title: 'Everything You Need',
-      text: 'We focus on DevOps, giving you more time to focus on your product',
-      icon: 'mdi-arrow-right-drop-circle'
+      title: "Everything You Need",
+      text: "We focus on DevOps, giving you more time to focus on your product",
+      icon: "mdi-arrow-right-drop-circle"
     },
     {
-      title: 'Processing',
-      text: 'From virtual machines with proven price/performance advantages to a fully managed app development platform.',
-      icon: 'mdi-server-network'
+      title: "Processing",
+      text:
+        "From virtual machines with proven price/performance advantages to a fully managed app development platform.",
+      icon: "mdi-server-network"
     },
     {
-      title: 'State-of-the-Art Networking',
-      text: 'State-of-the-art software-defined networking products on Alpha Software\'s private fiber network',
-      icon: 'mdi-chart-bubble'
+      title: "State-of-the-Art Networking",
+      text:
+        "State-of-the-art software-defined networking products on Alpha Software's private fiber network",
+      icon: "mdi-chart-bubble"
     },
     {
-      title: 'Fully Secured',
-      text: 'Control access and visibility to resources running on a platform protected by Alpha Software\'s security model',
-      icon: 'mdi-verified'
+      title: "Fully Secured",
+      text:
+        "Control access and visibility to resources running on a platform protected by Alpha Software's security model",
+      icon: "mdi-verified"
     },
     {
-      title: 'Design Feedback Tool',
-      text: 'Collaborate on user experience journeys with your team using Alpha Software\'s EnFocus tool',
-      icon: 'mdi-brush'
+      title: "Design Feedback Tool",
+      text:
+        "Collaborate on user experience journeys with your team using Alpha Software's EnFocus tool",
+      icon: "mdi-brush"
     },
     {
-      title: 'Real-Time Monitoring',
-      text: 'Monitoring, logging, and diagnostics and more, all in an easy to use web management console or mobile app.',
-      icon: 'mdi-chart-areaspline'
+      title: "Real-Time Monitoring",
+      text:
+        "Monitoring, logging, and diagnostics and more, all in an easy to use web management console or mobile app.",
+      icon: "mdi-chart-areaspline"
     }
   ],
   testimonials: [
     {
-      author: 'Elizabeth Marner',
-      title: 'CEO, Boca Cola',
-      quote: '"Using AlphaSoftware’s Cloud Platform, the campaign runs smoothly 24/7 and includes redundancy, failover techniques, backups and state-of-the-art monitoring. Plus, it’s affordable."'
+      author: "Elizabeth Marner",
+      title: "CEO, Boca Cola",
+      quote:
+        '"Using AlphaSoftware’s Cloud Platform, the campaign runs smoothly 24/7 and includes redundancy, failover techniques, backups and state-of-the-art monitoring. Plus, it’s affordable."'
     },
     {
-      author: 'Miguel Killorn',
-      title: 'CEO, Abocado',
-      quote: '"The ease, integration and scalability that Alpha Softwareoffers with Cloud Platform allow us to offer the best online grocery shopping around."'
+      author: "Miguel Killorn",
+      title: "CEO, Abocado",
+      quote:
+        '"The ease, integration and scalability that Alpha Softwareoffers with Cloud Platform allow us to offer the best online grocery shopping around."'
     }
   ]
-}
+};
