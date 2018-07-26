850f8fb7527a249fe8efb2a579f86cfeb4d6f2e5
Kaplaugher <kaplaugher94@gmail.com>
2018-07-26 11:51:18 -0400

    fixed tour issue

--- src/lang/en/views/index.js
+++ src/lang/en/views/index.js
@@ -1,11 +1,13 @@
-import About from './About'
-import Contact from './Contact'
-import Home from './Home'
-import Services from './Services'
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Services from "./Services";
+import Tour from "./Tour";
 
 export default {
   About,
   Contact,
   Home,
-  Services
-}
+  Services,
+  Tour
+};
