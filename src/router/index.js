38f510425013d13fb03a004cf811e145a7672847
Kaplaugher <kaplaugher94@gmail.com>
2018-11-18 17:47:47 -0500

    added history mode

--- src/router/index.js
+++ src/router/index.js
@@ -7,36 +7,35 @@
  */
 
 // Lib imports
-import Vue from 'vue'
-import VueAnalytics from 'vue-analytics'
-import Router from 'vue-router'
-import Paths from './paths'
+import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
+import Router from 'vue-router';
+import Paths from './paths';
 
-Vue.use(Router)
+Vue.use(Router);
 
 // Function to create routes
 // Is default lazy but can be changed
-function route (obj) {
+function route(obj) {
   return {
     path: `/${obj.path}`,
     meta: obj.meta,
-    component: () => import(
-      /* webpackChunkName: "routes" */
+    component: () =>
+      import(/* webpackChunkName: "routes" */
       /* webpackMode: "lazy-once" */
-      `@/views/${obj.view}.vue`
-    )
-  }
+      `@/views/${obj.view}.vue`)
+  };
 }
 
-const routes = Paths.map(r => route(r))
+const routes = Paths.map(r => route(r));
 
-routes.push({ path: '*', redirect: '/' })
+routes.push({ path: '*', redirect: '/' });
 
 // Create a new router
 const router = new Router({
-  mode: 'hash',
+  mode: 'history',
   routes
-})
+});
 
 // Bootstrap Analytics
 // Set in .env
@@ -48,7 +47,7 @@ if (process.env.GOOGLE_ANALYTICS) {
     autoTracking: {
       page: process.env.NODE_ENV !== 'development'
     }
-  })
+  });
 }
 
-export default router
+export default router;
